6aa02386e149fe1c4989d59a0d685c22826e6a7d
zry <zhengry22@mails.tsinghua.edu.cn>
2024-08-19 16:33:16 +0800

    fix: add relinearize in horner

--- ReLU_ckks.cu
+++ ReLU_ckks.cu
@@ -82,13 +82,13 @@ void horner(const auto &encoder, const Evaluator &evaluator, const RelinKeys &re
     size_t poly_deg = poly.get_degree();
     Plaintext plain_coeff;
     double largest_coeff = poly.get_coeff_by_rank(poly_deg);
+    //double largest_coeff = 1.0;
     cout << "First coeff is: " << largest_coeff << endl;
     encoder.encode_float64_single(largest_coeff, std::nullopt, scale, plain_coeff);
     evaluator.multiply_plain_inplace(my_cipher, plain_coeff);
     evaluator.rescale_to_next_inplace(my_cipher); // a_n*x
 
     for (int i = poly_deg - 1; i >= 0; i--) {
-        cout << "deg = " << i << endl;
         if (i != poly_deg - 1) {
             // multiply x
             Ciphertext this_x = x_encrypted;
@@ -97,12 +97,13 @@ void horner(const auto &encoder, const Evaluator &evaluator, const RelinKeys &re
             this_x.scale() = my_cipher.scale();
             // Second multiply
             evaluator.multiply_inplace(my_cipher, this_x);
+            evaluator.relinearize_inplace(my_cipher, relin_keys);
             evaluator.rescale_to_next_inplace(my_cipher);
         }
         // Add const
-        cout << "Add const " << poly.get_coeff_by_rank((size_t)(i)) << endl;
         Plaintext this_coeff;
         encoder.encode_float64_single(poly.get_coeff_by_rank((size_t)(i)), std::nullopt, my_cipher.scale(), this_coeff);
+        //encoder.encode_float64_single(0.0, std::nullopt, my_cipher.scale(), this_coeff);
         evaluator.add_plain_inplace(my_cipher, this_coeff);
     }
 
@@ -190,9 +191,9 @@ int main() {
     Ciphertext x_encrypted;
     encryptor.encrypt_asymmetric(x_plain, x_encrypted);
     Ciphertext encrypted_result;
-    approx_with_fix(encoder, evaluator, relin_keys, scale, x_encrypted, encrypted_result);
-    //cout << "In horner!" << endl;
-    //horner(encoder, evaluator, relin_keys, scale, poly, x_encrypted, encrypted_result);
+    //approx_with_fix(encoder, evaluator, relin_keys, scale, x_encrypted, encrypted_result);
+    cout << "In horner!" << endl;
+    horner(encoder, evaluator, relin_keys, scale, poly, x_encrypted, encrypted_result);
 
     /*
     Decrypt, decode, and print the result.
