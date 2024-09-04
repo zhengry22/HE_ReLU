2db7d49f11c6099bfc43b500f2d641c253dac35d
zry <zhengry22@mails.tsinghua.edu.cn>
2024-09-04 15:24:04 +0800

    feat: add Paralell threads without optimizing

--- ReLU_ckks.cu
+++ ReLU_ckks.cu
@@ -6,10 +6,15 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
+#include <codecvt>
+#include <locale>
+//#include "./matplotlib-cpp/matplotlibcpp.h"
+//#define MYDEBUG
 #define BATCH_SIZE 40
+#define PARTS 2
 using namespace troy;
 using namespace std;
-
+//namespace plt = matplotlibcpp;
 /*
     Try to calculate the cipher with a fixed polynomial 
 */ 
@@ -70,8 +75,8 @@ void approx_with_fix(const CKKSEncoder &encoder, const Evaluator &evaluator, con
 }
 
 
-void horner(const auto &encoder, const Evaluator &evaluator, const RelinKeys &relin_keys, const double &scale,
-            const Polynomial<double> poly, const Ciphertext &x_encrypted, Ciphertext &encrypted_result) {
+void horner(const CKKSEncoder &encoder, const Evaluator &evaluator, const RelinKeys &relin_keys, const double &scale,
+            const Polynomial<double> &poly, const Ciphertext &x_encrypted, Ciphertext &encrypted_result) {
 
     /*
         Conduct the horner's algorithm in linear complexity
@@ -102,6 +107,38 @@ void horner(const auto &encoder, const Evaluator &evaluator, const RelinKeys &re
         encoder.encode_float64_single(poly.get_coeff_by_rank((size_t)(i)), std::nullopt, my_cipher.scale(), this_coeff);
         //encoder.encode_float64_single(0.0, std::nullopt, my_cipher.scale(), this_coeff);
         evaluator.add_plain_inplace(my_cipher, this_coeff);
+    }
+
+    encrypted_result = my_cipher;
+}
+
+
+void multiply_x(const CKKSEncoder &encoder, const Evaluator &evaluator, const RelinKeys &relin_keys, 
+    const double &scale, const int &degree, const Ciphertext &x_encrypted, Ciphertext &encrypted_result) {
+    Ciphertext my_cipher = x_encrypted; // set the initial value to be x
+
+    int deg_left = degree - 1; // Its x ^ 1 at the beginning
+    while (deg_left > 0) {
+        if (deg_left % 2 == 1) {
+            // multiply x
+            Ciphertext this_x = x_encrypted;
+            // First change scale 
+            evaluator.mod_switch_to_inplace(this_x, my_cipher.parms_id());
+            this_x.scale() = my_cipher.scale();
+            // Second multiply
+            evaluator.multiply_inplace(my_cipher, this_x);
+            evaluator.relinearize_inplace(my_cipher, relin_keys);
+            evaluator.rescale_to_next_inplace(my_cipher);
+            deg_left -= 1;
+        }
+        else {
+            Ciphertext tmp;
+            evaluator.square(my_cipher, tmp);
+            evaluator.relinearize_inplace(tmp, relin_keys);
+            evaluator.rescale_to_next_inplace(tmp);
+            my_cipher = tmp;
+            deg_left >>= 1;  
+        }    
     }
 
     encrypted_result = my_cipher;
@@ -184,27 +221,144 @@ int main() {
     Ciphertext x_encrypted;
     encryptor.encrypt_asymmetric(x_plain, x_encrypted);
     Ciphertext encrypted_result;
-    //approx_with_fix(encoder, evaluator, relin_keys, scale, x_encrypted, encrypted_result);
+
+    /*
+        Create multiple threads
+    */
+    vector<Polynomial<double>> fragments;
+    for (int i = 0; i < PARTS; i++) {
+        Polynomial<double> tmp = poly.slice((poly.get_degree() + 1) * i / PARTS, ((poly.get_degree() + 1) * (i + 1) / PARTS) - 1);
+        fragments.push_back(tmp);
+        tmp.check();
+    }
+
     auto start = std::chrono::high_resolution_clock::now();
-    horner(encoder, evaluator, relin_keys, scale, poly, x_encrypted, encrypted_result);
+    vector<Ciphertext> ciphers(2 * PARTS - 1);
+
+    vector<thread> threads;
+    mutex mtx;
+
+    for (int i = 0; i < (2 * PARTS - 1); i++) {
+        if (i % 2 == 0) {
+            threads.push_back(thread(horner, cref(encoder), cref(evaluator), cref(relin_keys), 
+                                    cref(scale), cref(fragments[i / 2]), cref(x_encrypted), ref(ciphers[i])));
+        } else {
+            int degree = poly.get_degree() + 1;
+            int part_degree = degree * (i + 1) / (PARTS * 2);
+            threads.push_back(thread(multiply_x, cref(encoder), cref(evaluator), cref(relin_keys), 
+                                    cref(scale), cref(part_degree), cref(x_encrypted), ref(ciphers[i])));
+        }
+    }
+
+    for (auto &t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+#ifdef MYDEBUG
+    cout << "Debugging..." << endl;
+    for (int i = 0; i < (2 * PARTS - 1); i++) {
+        cout << "i = " << i << endl;
+        Plaintext plain_result;
+        decryptor.decrypt(ciphers[i], plain_result);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        vector<complex<double>> result;
+        encoder.decode_complex64_simd(plain_result, result);
+        for (int j = 0; j < BATCH_SIZE; j++) {
+            //cout << "x: " << input[i].real() << "       Relu(x): " << relu(input[i].real()) << "      SiLU(x): " << silu(input[i].real()) << "       计算结果：" << result[i].real() << endl;
+            cout << "x: " << input[j].real() << "       Relu(x): " << relu(input[j].real()) << "       计算结果：" << result[j].real();
+            if (i % 2 == 0) {
+                cout << "     True value: " << fragments[i / 2].get_poly_value(input[j].real());
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << "Debugging end..." << endl;
+#endif
+
+    vector<thread> mult(PARTS - 1);
+
+    for (int i = 1; i < (2 * PARTS - 1); i += 2) {
+        // multiply x
+        mult.push_back(thread([&evaluator, &relin_keys, &ciphers, i]() mutable {
+            Ciphertext this_x = ciphers[i];
+            // First change scale 
+            evaluator.mod_switch_to_inplace(this_x, ciphers[i + 1].parms_id());
+            this_x.scale() = ciphers[i + 1].scale();
+            // Second multiply
+            evaluator.multiply_inplace(ciphers[i + 1], this_x);
+            evaluator.relinearize_inplace(ciphers[i + 1], relin_keys);
+            evaluator.rescale_to_next_inplace(ciphers[i + 1]);
+        }));
+    }
+
+    for (auto &t : mult) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+    Ciphertext enc;
+
+    
+
+    for (int i = 2; i < 2 * PARTS - 1; i += 2) {
+        Ciphertext this_x = ciphers[i - 2];
+        evaluator.mod_switch_to_inplace(this_x, ciphers[i].parms_id());
+        this_x.scale() = ciphers[i].scale();
+        evaluator.add_inplace(ciphers[i], this_x);
+        if ((i + 2) > (2 * PARTS - 1)) {
+            enc = ciphers[i];
+        }
+    }
+
+    //approx_with_fix(encoder, evaluator, relin_keys, scale, x_encrypted, encrypted_result);
+    
+    // horner(encoder, evaluator, relin_keys, scale, poly, x_encrypted, encrypted_result);
     
     /*
     Decrypt, decode, and print the result.
     */
     Plaintext plain_result;
-    decryptor.decrypt(encrypted_result, plain_result);
+    decryptor.decrypt(enc, plain_result);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     vector<complex<double>> result;
     encoder.decode_complex64_simd(plain_result, result);
     //print_vector(result, 16, 7);
+
+    /*
+        Ploting the curve: 
+    */
+
+    vector<double> x_coord, relu_y, poly_y;
     for (int i = 0; i < BATCH_SIZE; i++) {
         //cout << "x: " << input[i].real() << "       Relu(x): " << relu(input[i].real()) << "      SiLU(x): " << silu(input[i].real()) << "       计算结果：" << result[i].real() << endl;
-        //cout << "x: " << input[i].real() << "       Relu(x): " << relu(input[i].real()) << "    abs(x): " << abs_test(input[i].real()) << "       计算结果：" << result[i].real() << endl;
         cout << "x: " << input[i].real() << "       Relu(x): " << relu(input[i].real()) << "       计算结果：" << result[i].real() << endl;
+        x_coord.push_back(input[i].real());
+        relu_y.push_back(relu(input[i].real()));
+        poly_y.push_back(result[i].real());
     }
     std::cout << "运行时间: " << (double)duration.count() / (double)1000 << " ms" << std::endl;
-    cout << endl;
+    // Py_Initialize();
+    
+    // // 获取 Python 解释器路径
+    // const wchar_t* python_executable_w = Py_GetProgramName();
+    
+    // // 将 wchar_t* 转换为 std::string
+    // std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    // std::string python_executable = converter.to_bytes(python_executable_w);
+
+    // std::cout << "Python executable path: " << python_executable << std::endl;
+    
+    // Py_Finalize();
+
+    // plt::plot(x_coord, relu_y);
+    // plt::plot(x_coord, poly_y);  
+    // plt::show();
     return 0;
 
 }
