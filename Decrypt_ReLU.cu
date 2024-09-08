4ed7d2e31d980aef331459a2383e598030b22bc7
zry <zhengry22@mails.tsinghua.edu.cn>
2024-09-08 11:44:13 +0800

    feat: untrack several files

--- Decrypt_ReLU.cu
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include "ReLU_bfv.h"
-using namespace std;
-using namespace troy;
-
-int main() {
-    
-}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    unsigned long long test = -3;
-    cout << test;
-    return 0;
-}
